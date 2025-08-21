6ed121b6c691bd8c6e510a77cde279b043ebdb78
dmeetri <145245244+dmeetri@users.noreply.github.com>
2025-08-21 17:57:38 +0900

    Update SAY HALLO

--- kai-desc/cmd_manager/parcer_commands.py
+++ kai-desc/cmd_manager/parcer_commands.py
@@ -6,10 +6,21 @@ class ParceCommand:
         self.cmd = cmd
         self.model = fasttext.load_model("ai_models/commands_model.bin")
 
+        self.command_methods = {
+            "say_hallo": self.say_hallo,
+        }
+
     def parce(self):
         labels, probabilities = self.model.predict(self.cmd)
+        labels = [label.replace("__label__", "") for label in labels]
         probabilities = np.asarray(probabilities)
-        print(f"FAST - {labels}, {probabilities}")
+
+        if not labels: return
+
+        if labels[0] in self.command_methods:
+            self.command_methods[labels[0]]()
+        else:
+            print("Команда не найдена")
 
     def say_hallo(self):
         print('FASTTEXT - HALLO')
